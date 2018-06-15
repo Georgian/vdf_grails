e47a1dfd57732077dfffe470167e7fa9a7ba203f
Georgian Grec <georgian.grec@gmail.com>
2018-06-16 00:15:44 +0300

    Revert "Revert "Revert "s2-quickstart"""
    This reverts commit 286c32ddb6977efdb75e75e68c072549fbc1e7cd.

--- grails-app/domain/vdf_grails/VDFAthlete.groovy
+++ grails-app/domain/vdf_grails/VDFAthlete.groovy
@@ -1,33 +1,10 @@
 package vdf_grails
 
-import groovy.transform.EqualsAndHashCode
-import groovy.transform.ToString
-import grails.compiler.GrailsCompileStatic
+class VDFAthlete {
 
-@GrailsCompileStatic
-@EqualsAndHashCode(includes='username')
-@ToString(includes='username', includeNames=true, includePackage=false)
-class VDFAthlete implements Serializable {
-
-    private static final long serialVersionUID = 1
-
-    String username
-    String password
-    boolean enabled = true
-    boolean accountExpired
-    boolean accountLocked
-    boolean passwordExpired
-
-    Set<Role> getAuthorities() {
-        (VDFAthleteRole.findAllByVDFAthlete(this) as List<VDFAthleteRole>)*.role as Set<Role>
-    }
-
+	String email
+	String name
+	
     static constraints = {
-        password nullable: false, blank: false, password: true
-        username nullable: false, blank: false, unique: true
-    }
-
-    static mapping = {
-	    password column: '`password`'
     }
 }
