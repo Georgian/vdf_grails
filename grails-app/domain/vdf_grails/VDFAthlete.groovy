7b504971b500370468f9ef685c133377d917b0a6
Georgian Grec <georgian.grec@gmail.com>
2018-06-15 16:48:38 +0300

    s2-quickstart

--- grails-app/domain/vdf_grails/VDFAthlete.groovy
+++ grails-app/domain/vdf_grails/VDFAthlete.groovy
@@ -1,10 +1,33 @@
 package vdf_grails
 
-class VDFAthlete {
+import groovy.transform.EqualsAndHashCode
+import groovy.transform.ToString
+import grails.compiler.GrailsCompileStatic
 
-	String email
-	String name
-	
+@GrailsCompileStatic
+@EqualsAndHashCode(includes='username')
+@ToString(includes='username', includeNames=true, includePackage=false)
+class VDFAthlete implements Serializable {
+
+    private static final long serialVersionUID = 1
+
+    String username
+    String password
+    boolean enabled = true
+    boolean accountExpired
+    boolean accountLocked
+    boolean passwordExpired
+
+    Set<Role> getAuthorities() {
+        (VDFAthleteRole.findAllByVDFAthlete(this) as List<VDFAthleteRole>)*.role as Set<Role>
+    }
+
     static constraints = {
+        password nullable: false, blank: false, password: true
+        username nullable: false, blank: false, unique: true
+    }
+
+    static mapping = {
+	    password column: '`password`'
     }
 }
